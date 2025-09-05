d43d9ee979d8b49b92744b69858cb335c1a095e0
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-05 19:01:37 +0530

    update login page

--- editron/modules/auth/components/user-button.tsx
+++ editron/modules/auth/components/user-button.tsx
@@ -21,25 +21,25 @@ const UserButton = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <div className={cn("relative rounded-full")}>
-          <Avatar>
+        <div className={cn("relative rounded-full ring-2 ring-cyan-500/20 hover:ring-cyan-400/40 transition-all duration-200")}>
+          <Avatar className="border-2 border-cyan-500/30">
             <AvatarImage src={user?.image!} alt={user?.name!} />
-            <AvatarFallback className="bg-red-500">
+            <AvatarFallback className="bg-gradient-to-br from-cyan-500 to-blue-600 text-white">
               <User className="text-white" />
             </AvatarFallback>
           </Avatar>
         </div>
       </DropdownMenuTrigger>
 
-    <DropdownMenuContent className="mr-4">
-      <DropdownMenuItem>
+    <DropdownMenuContent className="mr-4 bg-gray-900/95 border-cyan-500/30 backdrop-blur-xl">
+      <DropdownMenuItem className="text-gray-200 focus:bg-cyan-500/10 focus:text-cyan-300">
         <span>
           {user?.email}
         </span>
       </DropdownMenuItem>
-      <DropdownMenuSeparator/>
+      <DropdownMenuSeparator className="bg-cyan-500/20"/>
         <LogoutButton>
-            <DropdownMenuItem>
+            <DropdownMenuItem className="text-gray-200 focus:bg-red-500/10 focus:text-red-300">
                 <LogOut className="h-4 w-4 mr-2"/>
                 LogOut
             </DropdownMenuItem>
